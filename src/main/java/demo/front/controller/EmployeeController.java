888813bdcc68b829b7a68049aff10f465b8d4aa2
GRAPPIN Antoine <agrappin@insitaction.com>
2024-03-18 17:25:59 +0100

    Suite front

--- src/main/java/demo/front/controller/EmployeeController.java
+++ src/main/java/demo/front/controller/EmployeeController.java
@@ -2,11 +2,15 @@ package demo.front.controller;
 
 import demo.front.model.Employee;
 import demo.front.service.EmployeeService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,5 +31,26 @@ public class EmployeeController {
         Employee employee = employeeService.getEmployee(id);
         model.addAttribute("employee", employee);
         return "employee";
+    }
+
+    @GetMapping("/add_employee")
+    public String addEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "add_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public ModelAndView saveEmployee(@ModelAttribute Employee employee) {
+        employeeService.saveEmployee(employee);
+        return new ModelAndView("redirect:/");
+    }
+
+    @GetMapping("/error")
+    public String getError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null){
+            model.addAttribute("error_code", Integer.valueOf(status.toString()));
+        }
+        return "error";
     }
 }
